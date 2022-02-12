5a7ac1879ea766df99438c130e8496aec2ade931
Bruno Dubal Hoffmann <bergsonall@outlook.com>
2022-02-12 00:35:18 -0300

    Update velha.py

--- velha.py
+++ velha.py
@@ -1,5 +1,5 @@
-from distutils.command.clean import clean
 from random import randint
+from re import T
 from turtle import clear
 
 print('Jogo da Velha')
@@ -7,6 +7,16 @@ print('Jogo da Velha')
 layout = [[' ', ' ', ' '],
 [' ', ' ', ' '],
 [' ', ' ', ' ']]
+
+
+def verifica_empate():
+    resultado = True
+    for l in layout:
+        for c in l:
+            if ' ' in c:
+                resultado = False
+    return resultado
+                
 
 def tabela():
     for lin in layout:
@@ -23,7 +33,7 @@ while True:
     if jogador == 'X' or jogador == 'O':
         break
     else:
-        print('Opção invalida! Tente digitar X ou O.')
+        print('Opção invalida! Tente digitar Circulo ou O.')
 
 
 cont_jogador = 0
@@ -32,30 +42,75 @@ if jogador == 'X':
 else:
     cont_jogador = 2
 
+bot = int(input('''Gostaria de jogar contra um Bot? 
+[1]SIM
+[2]NÃO
+'''))
 
 while True:
-    print()
+
+    if layout[0][0] + layout[0][1] + layout[0][2] == 'XXX' or layout[0][0] + layout[1][0] + layout[2][0] == 'XXX':
+        print('Jogador Xis ganhou')
+        break
+    elif layout[1][0] + layout[1][1] + layout[1][2] == 'XXX' or layout[0][1] + layout[1][1] + layout[2][1] == 'XXX':
+        print('Jogador Xis ganhou')
+        break
+    elif layout[2][0] + layout[2][1] + layout[2][2] == 'XXX' or layout[0][2] + layout[1][2] + layout[2][2] == 'XXX':
+        print('Jogador Xis ganhou')
+        break
+    elif layout[0][0] + layout[1][1] + layout[2][2] == 'XXX' or layout[0][2] + layout[1][1] + layout[2][0] == 'XXX':
+        print('Jogador Xis ganhou')
+        break
+
+
+    if layout[0][0] + layout[0][1] + layout[0][2] == 'OOO' or layout[0][0] + layout[1][0] + layout[2][0] == 'OOO':
+        print('Jogador Circulo ganhou')
+        break
+    elif layout[1][0] + layout[1][1] + layout[1][2] == 'OOO' or layout[0][1] + layout[1][1] + layout[2][1] == 'OOO':
+        print('Jogador Circulo ganhou')
+        break
+    elif layout[2][0] + layout[2][1] + layout[2][2] == 'OOO' or layout[0][2] + layout[1][2] + layout[2][2] == 'OOO':
+        print('Jogador Circulo ganhou')
+        break
+    elif layout[0][0] + layout[1][1] + layout[2][2] == 'OOO' or layout[0][2] + layout[1][1] + layout[2][0] == 'OOO':
+        print('Jogador Circulo ganhou')
+        break
+
+
+    if verifica_empate():
+        print('Deu velha')
+        break
+
+
     if cont_jogador == 1:
-        jogador = 'X'
-        while True:
-            print(f'Faça sua jogada com {jogador} ')
-            l = int(input('Digite a linha: '))
-            c = int(input('Digite a coluna: '))
-            if ' ' in layout[l-1][c-1]:
-                layout[l-1][c-1] = jogador
-                tabela()
-                cont_jogador += 1
-                break
-            else:
-                print('Campo ja contem uma jogada. Tente outro campo.')
-    else:
-        jogador = 'O'
-        while True:
-            print(f'Faça sua jogada com {jogador} ')
-            l = int(input('Digite a linha: '))
-            c = int(input('Digite a coluna: '))
-            if ' ' in layout[l-1][c-1]:
-                layout[l-1][c-1] = jogador
-                tabela()
-                cont_jogador -= 1
-                break
+        try:
+            while True:
+                print(f'Faça sua jogada com {jogador} ')
+                l = int(input('Digite a linha: '))
+                c = int(input('Digite a coluna: '))
+                if ' ' in layout[l-1][c-1]:
+                    layout[l-1][c-1] = jogador
+                    tabela()
+                    cont_jogador = 2
+                    break
+                else:
+                    print('Campo ja contem uma jogada. Tente outro campo.')
+        except:
+            print('Opção invalida. Por favor tente novamente')
+
+
+    elif cont_jogador == 2:
+        try:
+            while True:
+                print(f'Faça sua jogada com {jogador} ')
+                l = int(input('Digite a linha: '))
+                c = int(input('Digite a coluna: '))
+                if ' ' in layout[l-1][c-1]:
+                    layout[l-1][c-1] = jogador
+                    tabela()
+                    cont_jogador = 1
+                    break
+                else:
+                    print('Campo ja contem uma jogada. Tente outro campo.')
+        except:
+            print('Opção invalida. Por favor tente novamente.')
